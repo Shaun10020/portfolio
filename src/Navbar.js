5a938640bd379c07de0bd09d23a09da608c9a4d1
Shaun10020 <10020guava@gmail.com>
2023-07-31 13:19:54 -0700

    update background color

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom"
 
 const Navbar =()=>{
       return (
-            <div class="flex justify-start bg-orange-500">
-                  <Link class="p-2 text-black font-mono font-bold" to="/">Home</Link>
-                  <Link class="p-2 text-black font-mono font-bold" to="/about">About</Link>
-                  <Link class="p-2 text-black font-mono font-bold" to="/careers">Careers</Link>
-                  <Link class="p-2 text-black font-mono font-bold" to="/contact">Contact</Link>
-                  <Link class="p-2 text-black font-mono font-bold" to="/education">Education</Link>
-                  <Link class="p-2 text-black font-mono font-bold" to="/projects">Projects</Link>
-                  <Link class="p-2 text-black font-mono font-bold" to="/skills">Skills</Link>
+            <div class="flex items-center justify-start flex-wrap bg-orange-500">
+                  <Link class="p-4 text-black font-mono font-bold" to="/">Home</Link>
+                  <Link class="p-4 text-black font-mono font-bold" to="/about">About</Link>
+                  <Link class="p-4 text-black font-mono font-bold" to="/careers">Careers</Link>
+                  <Link class="p-4 text-black font-mono font-bold" to="/contact">Contact</Link>
+                  <Link class="p-4 text-black font-mono font-bold" to="/education">Education</Link>
+                  <Link class="p-4 text-black font-mono font-bold" to="/projects">Projects</Link>
+                  <Link class="p-4 text-black font-mono font-bold" to="/skills">Skills</Link>
             </div>
       )
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 function Home() {
     return (
       <div>
+        Hi! This is Home!
       </div>
     );
   }
