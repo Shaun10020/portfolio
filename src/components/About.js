b7d645ef6d71886eb0470ccda26cd0bcbdf832eb
Shaun10020 <10020guava@gmail.com>
2023-08-08 20:56:12 -0700

    support for sm

--- src/components/About.js
+++ src/components/About.js
@@ -12,13 +12,13 @@ function About() {
               <img src={face} class="h-60 border-2" alt="face" />
             </div>
             <div class="card">
-              <p class="p-3 font-semibold text-xl text-justify indent-10" >
+              <p class="card-text" >
                 I was born and raised in Malaysia, where I completed both my primary and secondary education. In 2020, I obtained my bachelor's degree in Mechatronics Engineering, and currently, I am pursuing further education in Computer Science at Oregon State University
               </p>
-              <p class="p-3 font-semibold text-xl text-justify indent-10" >
+              <p class="card-text" >
                 For my personality, I am introverted and often described as a quiet person. However, that is just a tip of my personality and I tend to show more when we know each other more. My friends always said I am a different person after we start hanging out, I smile a lot and has sense of humor.
               </p>
-              <p class="p-3 font-semibold text-xl text-justify indent-10" >
+              <p class="card-text" >
                 As for my interests, I like reading fictions and watch Sci-Fi TV shows. I found stories to be fascinating as they have different world background, history and such. Moreover, I enjoy learning new skills and knowledge, and always excited to try new stuff.
               </p>
             </div>
