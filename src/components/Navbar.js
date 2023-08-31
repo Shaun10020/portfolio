e1fa68df60ae09277144e81abc777dadd73f32df
Shaun10020 <10020guava@gmail.com>
2023-08-30 21:12:07 -0700

    mobile support

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,38 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { AiOutlineMenu } from "react-icons/ai";
+
+
+function toggleHide() {
+      var x = document.getElementById("navbar");
+      if (x.classList.contains("hidden")) 
+      {
+            x.classList.remove("hidden");
+            x.classList.add("flex");
+      } 
+      else 
+      {
+            x.classList.add("hidden");
+            x.classList.remove("flex");
+      }
+    }
+  
 
 const Navbar =()=>{
       return (
-            <div class="flex flex-col sm:flex-row items-center justify-start flex-wrap bg-orange-500">
-                  {/* <Link class="p-4 text-black font-mono font-bold" to="/">Home</Link>
-                  <Link class="p-4 text-black font-mono font-bold" to="/about">About</Link> */}
-                  <Link class="p-4 text-black font-mono font-bold" to="/">About</Link>
-                  <Link class="p-4 text-black font-mono font-bold" to="/careers">Careers</Link>
-                  <Link class="p-4 text-black font-mono font-bold" to="/education">Education</Link>
-                  <Link class="p-4 text-black font-mono font-bold" to="/projects">Projects</Link>
-                  <Link class="p-4 text-black font-mono font-bold" to="/skills">Skills</Link>
-                  <Link class="p-4 text-black font-mono font-bold" to="/contact">Contact</Link>
+            <div class="bg-orange-500">
+                  <div class="md:hidden p-2 flex justify-center" onClick={toggleHide}>
+                        <AiOutlineMenu/>
+                  </div>
+                  <div id="navbar" class="hidden md:flex flex-col sm:flex-row items-center justify-start flex-wrap">
+                        {/* <Link class="p-4 text-black font-mono font-bold" to="/">Home</Link>
+                        <Link class="p-4 text-black font-mono font-bold" to="/about">About</Link> */}
+                        <Link class="p-4 text-black font-mono font-bold" to="/">About</Link>
+                        <Link class="p-4 text-black font-mono font-bold" to="/careers">Careers</Link>
+                        <Link class="p-4 text-black font-mono font-bold" to="/education">Education</Link>
+                        <Link class="p-4 text-black font-mono font-bold" to="/projects">Projects</Link>
+                        <Link class="p-4 text-black font-mono font-bold" to="/skills">Skills</Link>
+                        <Link class="p-4 text-black font-mono font-bold" to="/contact">Contact</Link>
+                  </div>
             </div>
       )
 }
