528b32c620c49065375568b1ad4e7a8958253f37
Shaun10020 <10020guava@gmail.com>
2024-11-10 04:07:06 -0800

    update color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,19 +19,19 @@ function toggleHide() {
 
 const Navbar =()=>{
       return (
-            <div class="bg-orange-500">
+            <div class="navbar">
                   <div class="md:hidden p-2 flex justify-center" onClick={toggleHide}>
                         <AiOutlineMenu/>
                   </div>
                   <div id="navbar" class="hidden md:flex flex-col sm:flex-row items-center justify-start flex-wrap">
                         {/* <Link class="p-4 text-black font-mono font-bold" to="/">Home</Link>
                         <Link class="p-4 text-black font-mono font-bold" to="/about">About</Link> */}
-                        <Link class="p-4 text-black font-mono font-bold" to="/">About</Link>
-                        <Link class="p-4 text-black font-mono font-bold" to="/careers">Careers</Link>
-                        <Link class="p-4 text-black font-mono font-bold" to="/education">Education</Link>
-                        <Link class="p-4 text-black font-mono font-bold" to="/projects">Projects</Link>
-                        <Link class="p-4 text-black font-mono font-bold" to="/skills">Skills</Link>
-                        <Link class="p-4 text-black font-mono font-bold" to="/contact">Contact</Link>
+                        <Link class="p-4 font-mono font-bold" to="/">About</Link>
+                        <Link class="p-4 font-mono font-bold" to="/careers">Careers</Link>
+                        <Link class="p-4 font-mono font-bold" to="/education">Education</Link>
+                        <Link class="p-4 font-mono font-bold" to="/projects">Projects</Link>
+                        <Link class="p-4 font-mono font-bold" to="/skills">Skills</Link>
+                        <Link class="p-4 font-mono font-bold" to="/contact">Contact</Link>
                   </div>
             </div>
       )
